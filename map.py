b749158d75bc126fc2bef9c6aadd487bdd9e3551
Daltonshults <Dshults@cocc.edu>
2024-04-16 14:40:07 -0700

    added get_city_node function.

---
+++ map.py
@@ -0,0 +1,18 @@
+class CountryMap:
+    def __init__(self, city_nodes):
+        self.graph = {}
+        self.city_nodes = city_nodes
+
+    def create_graph(self):
+        for city in self.city_nodes:
+            self.graph[city.city_name] = {}
+
+            for go_city in city.go_cities_with_weights:
+                self.graph[city.city_name][go_city[0]] = go_city[1]
+
+    def get_graph(self):
+        return self.graph
+    
+    def get_city_node(self, city_name):
+        if city_name in self.graph.keys():
+            return self.graph[city_name]
