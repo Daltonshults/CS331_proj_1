3673317aaa1c9b6af4ea4cd8542d6490a6491977
Daltonshults <Dshults@cocc.edu>
2024-04-19 11:47:33 -0700

    Created a BFS class.

---
+++ bfs.py
@@ -0,0 +1,28 @@
+from node import Node
+from queue import Queue  as q
+
+class BreadthFristSearch:
+    def __init__(self, country_map) -> None:
+        self.frontier = q()
+        self.reached = []
+        self.map = country_map
+
+    def search(self, intitial, goal):
+        ...
+        # Initialize first node ROOT
+
+        #If the node is the goal node return node
+
+            # Add node to frontier, FIFO queue
+
+            # Add node to reached set/list
+
+            # While frontier is not empty
+
+                # Node = frontier.pop()
+
+                # For each child of the node
+
+                    # if node iw the goal node return
+
+                    # if node not in reached set add node to reached
