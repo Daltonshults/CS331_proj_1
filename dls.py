3081fa25053f4183bca273f532148dc51e5341a0
Daltonshults <Dshults@cocc.edu>
2024-04-20 11:27:59 -0700

    Commented out the print statements.

--- dls.py
+++ dls.py
@@ -29,8 +29,8 @@ class DepthLimitedSearch:
         '''
         neighbors = self.map.get_neighbors(current_node.get_state())
         nieghbor_nodes = []
-        print(f"Current Node: {current_node.get_state()}\n")
-        print(f"Neighbors: {neighbors}\n\n")
+        # print(f"Current Node: {current_node.get_state()}\n")
+        # print(f"Neighbors: {neighbors}\n\n")
 
         for i in neighbors.keys():
             node = CityNode(state=i,
@@ -59,22 +59,22 @@ class DepthLimitedSearch:
     def depth_limited_search(self, initial, goal, max_depth):
         # Add initial node to the frontier using a LIFO queue
         self.frontier.put(CityNode(initial, None, None, 0))
-        print(f"Goal: {goal}\n\n")
+        # print(f"Goal: {goal}\n\n")
 
         # Make result = failure
-        print("result = None\n\n")
+        # print("result = None\n\n")
         result = None
 
         # while frontier is not empty
         i = 0
         while not self.frontier.empty():
-            print(f"i: {i}")
+            # print(f"i: {i}")
             i += 1
             # Node = frontier.pop()
             node, path = self.pop_node_path()
 
-            print(node.get_state())
-            print(f"path: {path}")
+            # # # print(node.get_state())
+            # # print(f"path: {path}")
 
             # if node is the goal return the node
             if node.get_state() == goal:
@@ -83,12 +83,12 @@ class DepthLimitedSearch:
             # if the depth of node is greater than max_depth 
             if self.depth(node) > max_depth:
                 # Result = 'cutoff'
-                print("result = cutoff\n\n")
+                # print("result = cutoff\n\n")
                 result = "cutoff"
             
             # elif if it is not a cycle
             elif node.get_state() not in path:
-                print(f"Node state not in path: {node.get_state()}\n")
+                # print(f"Node state not in path: {node.get_state()}\n")
 
                 # for each child of the node expand()
                 for child in self.expand(node):
@@ -109,7 +109,7 @@ class IterativeDepthLimitedSearch:
 
     def iterative_depth_limited_search(self, initial, goal, max_depth):
         for depth in range(0, max_depth+1):
-            print(f"Depth: {depth}\n\n")
+            # print(f"Depth: {depth}\n\n")
             result = self.dls.depth_limited_search(initial, goal, depth)
 
             if result != "cutoff" and result != None:
