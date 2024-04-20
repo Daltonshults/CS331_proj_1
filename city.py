988e80171fa9bd7037f5e37ca53c651d7277cb5b
Daltonshults <Dshults@cocc.edu>
2024-04-20 12:35:47 -0700

    Added getters.

--- city.py
+++ city.py
@@ -4,3 +4,12 @@ class City:
         self.city_name = city_name
         self.coordinates = coordinates
         self.go_cities_with_weights = go_cities_with_weights
+
+    def get_city_name(self):
+        return self.city_name
+    
+    def get_coordinates(self):
+        return self.coordinates 
+    
+    def get_go_cities_with_weights(self):
+        return self.go_cities_with_weights
