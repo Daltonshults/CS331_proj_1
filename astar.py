c9375879e11acc1cabee0f54f03b1700599f21a9
Daltonshults <Dshults@cocc.edu>
2024-04-21 12:15:46 -0700

    Had the heuristic calculating the h(n) incorrectly. I was calculating between this node, and the current node. When I should be looking at the distance between node s_prime, and the goal.

--- astar.py
+++ astar.py
@@ -8,7 +8,7 @@ class AStarSearch:
         self.reached = {}
         self.map = country_map
 
-    def expand(self, current_node, city_to_weight_map, city_to_cords, heuristic):
+    def expand(self, current_node, city_to_weight_map, city_to_cords, goal, heuristic):
         nodes = []
 
         state = current_node.get_state()
@@ -16,10 +16,11 @@ class AStarSearch:
 
         for neighbor in neighbors:
             s_prime = neighbor[0]
-
+            
             cost = int(neighbor[1]) + current_node.get_path_cost()
-            action =  + heuristic(point_1 = city_to_cords[s_prime],
-                                  point_2 = city_to_cords[current_node.get_state()])
+            # Wrong should be between s_prime and goal
+            action = heuristic(point_1 = city_to_cords[s_prime],
+                                  point_2 = city_to_cords[goal])
             f_score = cost + action
             
             new_node = CityNodeAStar(s_prime, current_node, action, cost, f_score=f_score)
@@ -42,7 +43,7 @@ class AStarSearch:
             if node.get_state() == goal:
                 return node
 
-            for child in self.expand(node, city_to_weight_map, city_to_coords, heuristic):
+            for child in self.expand(node, city_to_weight_map, city_to_coords, goal, heuristic):
                 s = child.get_state()
 
                 if s not in self.reached or child.get_path_cost() < self.reached[s].get_path_cost():
