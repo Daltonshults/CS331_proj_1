d23f201d4d0567cd04e0d2be4378b4725dc70cb5
Daltonshults <Dshults@cocc.edu>
2024-04-21 12:28:01 -0700

    Updated heuristic to accurately measure between s_prime and goal. Also, adjusted to include h_score CityNodeAStar instance var.

--- astar.py
+++ astar.py
@@ -19,18 +19,30 @@ class AStarSearch:
             
             cost = int(neighbor[1]) + current_node.get_path_cost()
             # Wrong should be between s_prime and goal
-            action = heuristic(point_1 = city_to_cords[s_prime],
+            h_score = heuristic(point_1 = city_to_cords[s_prime],
                                   point_2 = city_to_cords[goal])
-            f_score = cost + action
+            f_score = cost + h_score
             
-            new_node = CityNodeAStar(s_prime, current_node, action, cost, f_score=f_score)
+            new_node = CityNodeAStar(state = s_prime,
+                                     parent= current_node,
+                                     action="Explored",
+                                     path_cost = cost,
+                                     h_score=h_score,
+                                     f_score=f_score)
             nodes.append(new_node)
 
         return nodes
 
     def astar_search(self, initial, goal, city_to_weight_map, city_to_coords, heuristic):
+        initial_h_score = heuristic(point_1 = city_to_coords[initial],
+                                    point_2 = city_to_coords[goal])
 
-        node = CityNodeAStar(initial, None, 0, 0)
+        node = CityNodeAStar(state = initial,
+                            parent=None,
+                            action="Initial Node",
+                            path_cost= 0,
+                            h_score = initial_h_score,
+                            f_score=initial_h_score)
 
         self.frontier.put(node)
 
@@ -47,6 +59,7 @@ class AStarSearch:
                 s = child.get_state()
 
                 if s not in self.reached or child.get_path_cost() < self.reached[s].get_path_cost():
+                    child.set_action("On Frontier")
                     self.reached[s] = child
                     self.frontier.put(child)
 
