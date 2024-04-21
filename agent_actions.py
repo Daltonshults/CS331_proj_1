4e1ed5c9a3c381d14783ba8b4f6bf69e1c0e2aa0
Daltonshults <Dshults@cocc.edu>
2024-04-21 13:49:35 -0700

    Agent actions are now being taken as they travel.

--- agent_actions.py
+++ agent_actions.py
@@ -1,6 +1,6 @@
 class AgentActions:
 
-    def move_city_a_to_city_b(self, city_a, city_b, node):
+    def move_city_a_to_city_b( city_a, city_b, node):
 
         action = f"Moved from {city_a} to {city_b}"
         node.set_action(action)
